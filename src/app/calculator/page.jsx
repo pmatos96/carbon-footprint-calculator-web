7fe63ee9685f0b51da490ca9850ce70e68771e39
Patrick Matos <pmatos96@hotmail.com>
2024-12-26 16:54:16 -0300

    feat: Adding an App bar

---
+++ src/app/calculator/page.jsx
@@ -0,0 +1,11 @@
+
+export default function CalculatorPage() {
+    return (
+      <div >
+        <main >
+          <h1>Calculate your Carbon Footprint:</h1>
+        </main>
+      </div>
+    );
+  }
+  
